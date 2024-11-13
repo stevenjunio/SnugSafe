3f36e6ef0ba8787a2750008d5560290dd87d1e37
Steven Junio <Steven.junio91@gmail.com>
2024-11-13 08:30:02 -0800

    fix error for static user fetch

--- frontend/src/hooks/useUserFiles.tsx
+++ frontend/src/hooks/useUserFiles.tsx
@@ -1,12 +1,15 @@
 import { FileItem } from "@/types/file.types";
+import { useCorbado } from "@corbado/react";
 import { useQuery } from "@tanstack/react-query";
 
 export function useUserFiles() {
+  const { user } = useCorbado();
+
   return useQuery({
     queryKey: ["userFiles"],
     queryFn: async (): Promise<FileItem[]> => {
       const response = await fetch(
-        `${import.meta.env.VITE_SERVER_URL}/api/v1/files?user=usr-4245673388096382210&page=1`
+        `${import.meta.env.VITE_SERVER_URL}/api/v1/files?user=${user.sub}&page=1`
       );
       return response.json();
     },
